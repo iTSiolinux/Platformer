26a25d4dfe441cb84b399988d4c2490546464b8e
Mkdir <79910206+iTSiolinux@users.noreply.github.com>
2023-11-09 17:03:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,36 @@
 
-const { Engine, Events, Render, Runner, Bodies, Body, Composite, World, Mouse, MouseConstraint } = Matter;
+const { Engine, Events, Render, Runner, Bodies, Body, Composite, World, Mouse, MouseConstraint } = Matter,
+engine = Engine.create(),
+runner = Runner.create();
 
 var width = window.innerWidth,
 height = window.innerHeight,
 scale = 16,
-data = Cookie.get("data");
-var currentSound = null;
-
-if (data){
-
-}
-
-
-// menu behavor
-const menu = document.getElementById('ui');
-
-// Add an event listener to the menu for hovering (so it doesn't collapse when you hover over it)
-menu.addEventListener('mouseover', () => {
-    menu.style.width = '150px'; // Expand the menu width
-});
-
-// Add an event listener to the menu for leaving the menu
-menu.addEventListener('mouseout', () => {
-    menu.style.width = '0'; // Collapse the menu width
-});
-
-
-
-const engine = Engine.create();
-const runner = Runner.create();
-
-var renderOptions =  {
+sound = [],
+renderOptions =  {
     width: 0,
     height: 0,
     wireframes: false,
     background: "lightblue",
     pixelRatio: 1
-}
-
+};
 
 if (window.innerHeight < 512 || window.innerWidth < 256) {
-    const message = "Sorry, your window is too small to display the content properly. Please resize your window for a better experience.";
-    window.alert(message);
+    window.alert("Sorry, your window is too small to display the content properly. Please resize your window for a better experience.");
     window.location.reload()
 } else {
     renderOptions.width = window.innerWidth * 0.95
     renderOptions.height = window.innerHeight * 0.95
-
 }
-  
-
 
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: renderOptions
 });
-
-
-const mouseConstraint = MouseConstraint.create(engine, {
-        mouse: Mouse.create(render.canvas),
-        constraint: {
-            stiffness: 0.1,
-            render: { visible: false }
-          }
-    });
-
-
-
-World.add(engine.world, mouseConstraint);
-
-var playerNode = null
-
-
-
-
 const GAME = {
     player: {
+        node: null,
         a: {
             radius: scale,
             render: {
@@ -115,6 +69,43 @@ const GAME = {
         volume: 1
     },
     zoom: 1,
+    Map: {
+        load: (map) => {
+            GAME.Map.map = map;
+            const blocks = map.blocks;
+            const platforms = map.platforms;
+            const bodies = []; 
+
+            platforms.forEach((plat) => {
+                if (plat.type === 'rect') {
+                    const block = blocks.find((block) => block.id === plat.id);
+                    if (block) {
+                        const options = {
+                            label: plat.label,
+                            isStatic: block.isStatic,
+                            render: block.render,
+                        }
+                        if (plat.group || block.group){
+                            options.collisionFilter.group = plat.group || block.group;
+                        }
+                        if (plat.label || block.label){
+                            options.label = plat.label || block.label;
+                        }
+                        const obj = Bodies.rectangle(plat.x, plat.y, block.scale.width, block.scale.height, options);
+                        bodies.push(obj);
+                    }
+                }
+            });
+            Composite.allBodies(engine.world).forEach((body) => {
+                if (body !== GAME.player.node) {
+                    Composite.remove(engine.world, body);
+                }
+            });
+            Composite.add(engine.world, bodies)
+            GAME.player = { ...GAME.player, ...map.player}
+            new Player()
+        }
+    }
 }
 
 var bgMusic = new Howl({
@@ -226,96 +217,24 @@ const level1 = {
     platforms: [
         { id: 1, label: null, type: 'rect', x: 0, y: 64 },
         { id: 1, label: null, type: 'rect', x: 128, y: 64 },
-
         { id: 1, label: null, type: 'rect', x: 384, y: 128 },
-
         { id: 1, label: null, type: 'rect', x: 640, y: 256 },
-
-
         { id: 2, label: '{ "interact" : {"script": "GAME.Map.load(levels_map)"} }', type: 'rect', x: 896, y: 192 },
     ]
 };
 
-
-GAME.Map = [];
-
-GAME.Map.load  = (map) => {
-    GAME.Map.map = map;
-    const blocks = map.blocks;
-    const platforms = map.platforms;
-    const bodies = []; 
-
-    platforms.forEach((plat) => {
-        if (plat.type === 'rect') {
-            const block = blocks.find((block) => block.id === plat.id);
-
-            if (block) {
-
-                const options = {
-                    label: plat.label,
-                    isStatic: block.isStatic,
-                    render: block.render,
-                }
-                if (plat.group || block.group){
-                    options.collisionFilter.group = plat.group || block.group;
-                }
-                if (plat.label || block.label){
-                    options.label = plat.label || block.label;
-                }
-                const obj = Bodies.rectangle(
-                    plat.x,
-                    plat.y,
-                    block.scale.width,
-                    block.scale.height, 
-                    options
-                );
-
-                bodies.push(obj);
-            }
-        }
-    });
-    Composite.allBodies(engine.world).forEach((body) => {
-        if (body !== playerNode) {
-            Composite.remove(engine.world, body);
-        }
-    });
-    Composite.add(engine.world, bodies)
-    GAME.player = { ...GAME.player, ...map.player}
-    new Player()
-}
-
-
-
-
-
-function locatePlayer(){
+function updateCamera(){
     w = render.options.width / 2
     h = render.options.height / 2
-    X = playerNode.body.position.x - w
-    Y = playerNode.body.position.y  - h
-    return {x: X, y: Y}
-}
+    X = GAME.player.node.body.position.x - w
+    Y = GAME.player.node.body.position.y  - h
 
-function voidKiller(){
-    const bodiesToRemove = [];
-    const voidLow = GAME.Map.map.PlayerAttributes.void.low;
-    Composite.allBodies(engine.world).forEach((body) => {
-        if (body.position.y > voidLow) {
-            bodiesToRemove.push(body);
-        }
-    });
-
-    Composite.remove(engine.world, bodiesToRemove);
-}
-
-function updateCamera(){
-    loc = locatePlayer()
     if (GAME.player.bools.camera.lockX && GAME.player.bools.camera.lockY){
-        render.context.setTransform(GAME.zoom, 0, 0, GAME.zoom, -loc.x, -loc.y)
+        render.context.setTransform(GAME.zoom, 0, 0, GAME.zoom, -X, -Y)
     } else if (GAME.player.bools.camera.lockX){
-        render.context.setTransform(GAME.zoom, 0, 0, GAME.zoom, -loc.x, 0)
+        render.context.setTransform(GAME.zoom, 0, 0, GAME.zoom, -X, 0)
     } else if (GAME.player.bools.camera.lockY){
-        render.context.setTransform(GAME.zoom, 0, 0, GAME.zoom, 0, -loc.y)
+        render.context.setTransform(GAME.zoom, 0, 0, GAME.zoom, 0, -Y)
     }
 }
 
@@ -353,31 +272,28 @@ function formatTimer(milliseconds) {
     return formattedTimer;
 }
 
-function home(){
-    GAME.Map.load(default_map)
-}
-
-function leave(){
-    if (GAME.Map.map.leaveMap){
-        GAME.Map.load(GAME.Map.map.leaveMap)
-    } else if (GAME.Map.map != default_map){
-        GAME.Map.load(default_map)
-    }
-}
-
 GAME.loop =  ()=>{
-    if (playerNode){
-        playerNode.update()
+    if (GAME.player.node){
+        GAME.player.node.update()
         updateCamera()
     }
     if (GAME.Map.map.PlayerAttributes.displayTimer){
         timer = document.getElementById("timer")
-        timer.innerText = `time: ${formatTimer(playerNode.aliveTime.getElapsedTime())}`
+        timer.innerText = `time: ${formatTimer(GAME.player.node.aliveTime.getElapsedTime())}`
     } else {
         timer = document.getElementById("timer")
         timer.innerText = ``
     }
-    voidKiller()
+    // killing voiding entitys.
+    const bodiesToRemove = [];
+    const voidLow = GAME.Map.map.PlayerAttributes.void.low;
+    Composite.allBodies(engine.world).forEach((body) => {
+        if (body.position.y > voidLow) {
+            bodiesToRemove.push(body);
+        }
+    });
+
+    Composite.remove(engine.world, bodiesToRemove);
 }
 
 function keySetBoolean(e, fill) {
@@ -385,8 +301,8 @@ function keySetBoolean(e, fill) {
         GAME.player.bools.movement.up = fill;
 
         if (GAME.player.bools.onGround && fill) {
-            const playerVelocity = playerNode.body.velocity;
-            Body.setVelocity(playerNode.body, { x: playerVelocity.x, y: -GAME.player.a.speed.y * 2 });
+            const playerVelocity = GAME.player.node.body.velocity;
+            Body.setVelocity(GAME.player.node.body, { x: playerVelocity.x, y: -GAME.player.a.speed.y * 2 });
             GAME.player.bools.onGround = false;
         }
     }
@@ -465,58 +381,45 @@ class Player {
             if (Label.collide && Label.collide.script) {
                 new Function(Label.collide.script)()
             }
-
             if (Label.interact && Label.interact.script && GAME.player.bools.movement.interact) {
                 new Function(Label.interact.script)()
             }
-            
-            
         }
-        
-        if (playerNode.body.position.y < otherBody.position.y){
+        if (GAME.player.node.body.position.y < otherBody.position.y){
             GAME.player.bools.onGround = true;
         }
-
     }
     constructor(){
-        
         this.body = new Bodies.circle(GAME.player.spawnPoint.x, GAME.player.spawnPoint.y, GAME.player.a.radius, {
             render: GAME.player.a.render,
             label: "player"
         })
-
         Composite.add(engine.world, this.body);
         this.aliveTime.start()
-        
         this.remove = () => {
             Composite.remove(engine.world, this.body);
         }
-
         this.move = () => {
-            const { x, y } = playerNode.body.velocity;
+            const { x, y } = GAME.player.node.body.velocity;
             if (GAME.player.bools.movement.right){
-                Body.setVelocity(playerNode.body, { x: GAME.player.a.speed.x, y });
+                Body.setVelocity(GAME.player.node.body, { x: GAME.player.a.speed.x, y });
             }
             if (GAME.player.bools.movement.left){
-                Body.setVelocity(playerNode.body, { x: -GAME.player.a.speed.x, y });
+                Body.setVelocity(GAME.player.node.body, { x: -GAME.player.a.speed.x, y });
             }
         }
-
-        if (playerNode){
-            playerNode.remove()
+        if (GAME.player.node){
+            GAME.player.node.remove()
         }
-        playerNode = this;
-        
+        GAME.player.node = this;
         Events.on(engine, 'collisionActive', (event) => {
-            
             const pairs = event.pairs;
             for (let i = 0; i < pairs.length; i++) {
                 const pair = pairs[i];
-        
-                if (pair.bodyA === playerNode.body) {
+                if (pair.bodyA === GAME.player.node.body) {
                     const otherBody = pair.bodyB; 
                     this.collide(otherBody);
-                } else if (pair.bodyB === playerNode.body) {
+                } else if (pair.bodyB === GAME.player.node.body) {
                     const otherBody = pair.bodyA;
                     this.collide(otherBody);
                 }
@@ -532,11 +435,10 @@ class Player {
 Render.run(render);
 Runner.run(runner, engine);
 setInterval(GAME.loop, 10)
-
 setTimeout(() => {
     GAME.Map.load(default_map)
     new Player();
-    currentSound = bgMusic.play()
+    sound.bgMusic = bgMusic.play()
 }, window.onload)
 
 // events handlers:
